68b9e81d7eee8e97486c0e2957aafb44258cc129
DFWilliams <williams.dee.f99@gamil.com>
2022-07-05 21:09:08 -0700

    adds logic to create winning and losing conditions

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
 import React, { Component } from 'react'
 import './App.css'
+import Square from './components/Square'
+
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      board: ["?", "?", "?", "?", "?", "?", "?", "?", "?"]
+      board: ["?", "?", "?", "?", "?", "?", "?", "?", "?"],
+      treasureLocation: null, 
+      bombLocation: null,
     }
   }
 
 
-handleGamePlay = (index) => {
-  // alert(index)
-  const { board }   = this.state
-  board[index] = "🌴"
-  this.setState({board: board})
-}
+  componentDidMount() {
+    let treasure = Math.floor(Math.random() * this.state.board.length)
+    let bomb = Math.floor(Math.random() * this.state.board.length)
+    this.setState({treasureLocation: treasure, bombLocation: bomb})
+  }
+
+
+  handleGamePlay = (index) => {
+    // alert(index)
+    const { board, treasureLocation, bombLocation } = this.state
+    if(index === treasureLocation) {
+      board[index] = "💎"
+      this.setState({board: board})
+    } else if(index === bombLocation) {
+      board[index] = "💣"
+      this.setState({board: board})
+    } else {
+      board[index] = "🌴"
+      this.setState({board: board})
+    }
+  }
 
 
   render() {
+    console.log("treasure", this.state.treasureLocation)
+    console.log("bomb", this.state.bombLocation)
     return(
       <>
         <h1>Treasure Hunt Game</h1>
-        <div className= 'gameboard'>
+        <div className='gameboard'>
         {this.state.board.map((value, index) => {
           return(
           <Square
